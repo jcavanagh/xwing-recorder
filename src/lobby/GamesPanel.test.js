a78a28b5cf8d15323b41b304ebdb15f273417153
Jeremy Hurwitz <jeremy.hurwitz@gmail.com>
2020-05-09 20:27:23 -0700

    Remove a bit of cruft that got left behind from debugging

--- src/lobby/GamesPanel.test.js
+++ src/lobby/GamesPanel.test.js
@@ -2,19 +2,6 @@ import React from 'react';
 import { cleanup, fireEvent, render } from '@testing-library/react';
 import GamesPanel from '../lobby/GamesPanel';
 
-// import firebase from '@firebase/testing';
-// const firebase = require('@firebase/testing');
-
-// const firebaseApp = firebase.initializeTestApp({
-//   databaseName: 'xwing-recorder',
-//   projectId: 'xwing-recorder',
-//   auth: { uid: 'TheTestUser' }
-// });
-
-// Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
-// unmount and cleanup DOM after the test is finished.
-afterEach(cleanup);
-
 describe('CreateGame only if logged in', () => {
   test('disabled if not logged in', () => {
     const { getByText } = render(<GamesPanel user={null} games={[]} createGame={null} />);
